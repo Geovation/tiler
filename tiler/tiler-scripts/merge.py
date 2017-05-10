2a7a32f8f1a43a76350202b7f7414fad7f1b927a
Shan-Chun Kuo <joy961406@gmail.com>
2017-05-10 11:35:41 +0100

    Unit test for merging tiles with overlap handling

--- tiler/tiler-scripts/merge.py
+++ tiler/tiler-scripts/merge.py
@@ -20,9 +20,9 @@ def copy_dir(root_src_dir, root_dst_dir):
 
     for src_dir, dirs, files in os.walk(root_src_dir):
         dst_dir = src_dir.replace(root_src_dir, root_dst_dir, 1)
-        
+
         if not os.path.exists(dst_dir):
-            os.makedirs(dst_dir)        
+            os.makedirs(dst_dir)
         for file_ in files:
             src_file = os.path.join(src_dir, file_)
             dst_file = os.path.join(dst_dir, file_)
